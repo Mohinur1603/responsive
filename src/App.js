c534c25df1e877862639dec9de8bba47994154cb
MokhinurM <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1~>
2022-09-15 06:11:28 +0300

    init

--- src/App.js
+++ src/App.js
@@ -45,268 +45,288 @@ function App() {
           </div>
         </div>
         <div className="line"></div>
-      </header>
-      <section>
-        <div className="container-fluid top pt-4 pb-2 px-5 d-flex justify-content-between align-items-center bg-light">
-          <div class="d-flex ps-5 gap-5">
-            <h5>Start Bootstrap</h5>
-            <ul className="ul d-flex gap-4">
+        <div className="lists d-flex justify-content-between px-5 py-2 bg-dark">
+          <h5 class="text-white">Start Bootstrap</h5>
+          <div>
+            <ul className="d-flex gap-3">
               <li>
-                <a
-                  style={{ color: "#333", fontWeight: "500" }}
-                  href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#!"
-                >
+                <a href="https://startbootstrap.github.io/startbootstrap-blog-home/#">
                   Home
                 </a>
               </li>
               <li>
-                <a href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#!">
+                <a href="https://startbootstrap.github.io/startbootstrap-blog-home/#">
                   About
                 </a>
               </li>
               <li>
-                <a href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#!">
-                  Shop{" "}
-                  <span className="ps-1" style={{ fontSize: "11px" }}>
-                    &#x25BC;
-                  </span>
+                <a href="https://startbootstrap.github.io/startbootstrap-blog-home/#">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a
+                  style={{ color: "#fff" }}
+                  href="https://startbootstrap.github.io/startbootstrap-blog-home/#"
+                >
+                  Blog
                 </a>
               </li>
             </ul>
           </div>
-          <div>
-            <button className="btn1 me-5">
-              <img
-                style={{ width: "17px" }}
-                src="https://cdn-icons-png.flaticon.com/512/665/665865.png"
-                alt="cart"
-              />
-              <span>Cart</span>{" "}
-              <img
-                style={{ width: "17px" }}
-                src="https://cdn-icons-png.flaticon.com/512/3840/3840570.png"
-                alt="2"
-              />
-            </button>
-          </div>
         </div>
-        <div className="landing">
-          <h1 style={{ fontSize: "62px", fontWeight: "700" }}>Shop in style</h1>
-          <p style={{ color: "#888", fontSize: "22px", fontWeight: "400" }}>
-            With this shop hompeage template
-          </p>
-        </div>
-      </section>
+      </header>
+      <div className="blogHome">
+        <span style={{ fontSize: "38px", fontWeight: "bold" }}>
+          Welcome to Blog Home!
+        </span>
+        <p className="text-dark opacity-75 fs-5">
+          A Bootstrap 5 starter layout for your next blog homepage
+        </p>
+      </div>
       <main>
-        <div class="card">
-          <span
-            className="boxCard"
-            style={{
-              width: "15rem",
-              height: "150px",
-              backgroundColor: "lightgrey",
-            }}
-          >
-            450 x 300
-          </span>
-          <div class="card-body p-4 text-center">
-            <h5 class="card-title">Funcy Product</h5>
-            <p class="card-text mb-5">$40.00-$80.00</p>
-            <a
-              href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#"
-              class="btn btn-outline-dark mt-4"
-            >
-              View options
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <span
-            className="boxCard"
-            style={{
-              width: "15rem",
-              position: "relative",
-              height: "150px",
-              backgroundColor: "lightgrey",
-            }}
-          >
-            450 x 300
-          </span>
-          <div className="abs">Sale</div>
-          <div class="card-body p-4 text-center">
-            <h5 class="card-title">Special Item</h5>
-            <span style={{ color: "yellow", fontSize: "22px" }}>
-              &#128970;&#128970;&#128970;&#128970;&#128970;
+        <div className="cards">
+          <div class="card w-100 mb-4">
+            <span className="img" style={{ height: "260px", fontSize: "60px" }}>
+              850 x 350
             </span>
-            <p class="card-text">
-              <del>$20.00</del> $18.00
-            </p>
+            <div class="card-body">
+              <span>January 1, 2022</span>
+              <h2 class="card-title">Featured Post Title</h2>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a
+                laboriosam. Dicta expedita corporis animi vero voluptate
+                voluptatibus possimus, veniam magni quis!
+              </p>
+              <a href="#" class="btn btn-primary">
+                Read more &#8594;
+              </a>
+            </div>
+          </div>
+          <div class="card mb-4 box1">
+            <span className="img">700 x 350</span>
+            <div class="card-body">
+              <span>January 1, 2022</span>
+              <h4 class="card-title"> Post Title</h4>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis aliquid atque, nulla.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Read more &#8594;
+              </a>
+            </div>
+          </div>
+          <div class="card mb-4 box1">
+            <span className="img">700 x 350</span>
+            <div class="card-body">
+              <span>January 1, 2022</span>
+              <h4 class="card-title"> Post Title</h4>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis aliquid atque, nulla.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Read more &#8594;
+              </a>
+            </div>
+          </div>
+          <div class="card mb-4 box1">
+            <span className="img">700 x 350</span>
+            <div class="card-body">
+              <span>January 1, 2022</span>
+              <h4 class="card-title"> Post Title</h4>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis aliquid atque, nulla.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Read more &#8594;
+              </a>
+            </div>
+          </div>
+          <div class="card mb-4 box1">
+            <span className="img">700 x 350</span>
+            <div class="card-body">
+              <span>January 1, 2022</span>
+              <h4 class="card-title"> Post Title</h4>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis aliquid atque, nulla.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Read more &#8594;
+              </a>
+            </div>
+          </div>
+          <div
+            class="btn-group btns"
+            role="group"
+            aria-label="Basic radio toggle button group"
+          >
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio1"
+              autocomplete="off"
+              checked
+            />
+            <label class="btn btn-outline-secondary" for="btnradio1">
+              Newer
+            </label>
 
-            <a
-              href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#"
-              class="btn btn-outline-dark mt-4"
-            >
-              Add to cart
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <span
-            className="boxCard"
-            style={{
-              width: "15rem",
-              height: "150px",
-              position: "relative",
-              backgroundColor: "lightgrey",
-            }}
-          >
-            450 x 300
-          </span>
-          <div className="abs">Sale</div>
-          <div class="card-body p-4 text-center">
-            <h5 class="card-title">Sale Item</h5>
-            <p class="card-text mb-5">
-              <del>$50.00</del> $25.00
-            </p>
-            <a
-              href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#"
-              class="btn btn-outline-dark mt-4"
-            >
-              Add to card
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <span
-            className="boxCard"
-            style={{
-              width: "15rem",
-              height: "150px",
-              backgroundColor: "lightgrey",
-            }}
-          >
-            450 x 300
-          </span>
-          <div class="card-body p-4 text-center">
-            <h5 class="card-title">Popular Item</h5>
-            <span style={{ color: "yellow", fontSize: "22px" }}>
-              &#128970;&#128970;&#128970;&#128970;&#128970;
-            </span>
-            <p class="card-text">$40.00</p>
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio2"
+              autocomplete="off"
+            />
+            <label class="btn btn-outline-secondary" for="btnradio2">
+              1
+            </label>
 
-            <a
-              href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#"
-              class="btn btn-outline-dark mt-4"
-            >
-              Add to cart
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <span
-            className="boxCard"
-            style={{
-              width: "15rem",
-              height: "150px",
-              position: "relative",
-              backgroundColor: "lightgrey",
-            }}
-          >
-            450 x 300
-          </span>
-          <div className="abs">Sale</div>
-          <div class="card-body p-4 text-center">
-            <h5 class="card-title">Sale Item</h5>
-            <p class="card-text mb-5">
-              <del>$50.00</del> $25.00
-            </p>
-            <a
-              href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#"
-              class="btn btn-outline-dark mt-4"
-            >
-              Add to card
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <span
-            className="boxCard"
-            style={{
-              width: "15rem",
-              height: "150px",
-              backgroundColor: "lightgrey",
-            }}
-          >
-            450 x 300
-          </span>
-          <div class="card-body p-4 text-center">
-            <h5 class="card-title">Funcy Product</h5>
-            <p class="card-text mb-5">$40.00-$80.00</p>
-            <a
-              href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#"
-              class="btn btn-outline-dark mt-4"
-            >
-              View options
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <span
-            className="boxCard"
-            style={{
-              width: "15rem",
-              height: "150px",
-              position: "relative",
-              backgroundColor: "lightgrey",
-            }}
-          >
-            450 x 300
-          </span>
-          <div className="abs">Sale</div>
-          <div class="card-body p-4 text-center">
-            <h5 class="card-title">Special Item</h5>
-            <span style={{ color: "yellow", fontSize: "22px" }}>
-              &#128970;&#128970;&#128970;&#128970;&#128970;
-            </span>
-            <p class="card-text">
-              <del>$20.00</del> $18.00
-            </p>
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio7"
+              autocomplete="off"
+            />
+            <label class="btn btn-outline-secondary" for="btnradio7">
+              2
+            </label>
 
-            <a
-              href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#"
-              class="btn btn-outline-dark mt-4"
-            >
-              Add to cart
-            </a>
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio3"
+              autocomplete="off"
+            />
+            <label class="btn btn-outline-secondary" for="btnradio3">
+              3
+            </label>
+
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio4"
+              autocomplete="off"
+            />
+            <label class="btn btn-outline-secondary" for="btnradio4">
+              ...
+            </label>
+
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio5"
+              autocomplete="off"
+            />
+            <label class="btn btn-outline-secondary" for="btnradio5">
+              15
+            </label>
+
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio6"
+              autocomplete="off"
+            />
+            <label class="btn btn-outline-secondary" for="btnradio6">
+              Older
+            </label>
           </div>
         </div>
-        <div class="card">
-          <span
-            className="boxCard"
-            style={{
-              width: "15rem",
-              height: "150px",
-              backgroundColor: "lightgrey",
-            }}
-          >
-            450 x 300
-          </span>
-          <div class="card-body p-4 text-center">
-            <h5 class="card-title">Popular Item</h5>
-            <span style={{ color: "yellow", fontSize: "22px" }}>
-              &#128970;&#128970;&#128970;&#128970;&#128970;
-            </span>
-            <p class="card-text">$40.00</p>
-
-            <a
-              href="https://startbootstrap.github.io/startbootstrap-shop-homepage/#"
-              class="btn btn-outline-dark mt-4"
-            >
-              Add to cart
-            </a>
+        <div className="forms">
+          <div class="card mb-4">
+            <div class="card-header">Search</div>
+            <div class="card-body">
+              <div class="input-group mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Enter search term..."
+                  aria-label="Recipient's username"
+                  aria-describedby="button-addon2"
+                />
+                <button
+                  class="btn btn-primary"
+                  type="button"
+                  id="button-addon2"
+                >
+                  Go!
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-4">
+            <div class="card-header">Categories</div>
+            <div class="card-body d-flex gap-5">
+              <div>
+                <a
+                  className="a"
+                  href="https://startbootstrap.github.io/startbootstrap-blog-home/#!"
+                >
+                  Web Design
+                </a>
+                <br></br>
+                <a
+                  className="a"
+                  href="https://startbootstrap.github.io/startbootstrap-blog-home/#!"
+                >
+                  HTML
+                </a>
+                <br></br>
+                <a
+                  className="a"
+                  href="https://startbootstrap.github.io/startbootstrap-blog-home/#!"
+                >
+                  Freebies
+                </a>
+              </div>
+              <div class="ms-4">
+                <a
+                  className="a"
+                  href="https://startbootstrap.github.io/startbootstrap-blog-home/#!"
+                >
+                  JavaScript
+                </a>
+                <br></br>
+                <a
+                  className="a"
+                  href="https://startbootstrap.github.io/startbootstrap-blog-home/#!"
+                >
+                  CSS
+                </a>
+                <br></br>
+                <a
+                  className="a"
+                  href="https://startbootstrap.github.io/startbootstrap-blog-home/#!"
+                >
+                  Tutorials
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-4">
+            <div class="card-header">Side Widget</div>
+            <div class="card-body">
+              <p>
+                You can put anything you want inside of these side widgets. They
+                are easy to use, and feature the Bootstrap 5 card component!
+              </p>
+            </div>
           </div>
         </div>
       </main>
+
       <footer>
         <p>Copyright © Your Website 2022</p>
       </footer>
